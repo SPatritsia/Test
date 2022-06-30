526bac99534b46217661f3c435c719450c7def30
SPatritsia <patricia.sich@gmail.com>
2022-06-30 10:45:40 +0300

    Test Automation

--- test/pageobjects/test.js
+++ test/pageobjects/test.js
@@ -4,11 +4,17 @@ class test extends Page {
 get allItems(){
     return $(' nav > ul > div:nth-child(3) > li > div > a > span')
 }
+get popupDiscountCloseBtn(){
+    return $('.klaviyo-close-form')
+}
+get elementBlock(){
+    return('div.products-list-grid-container > ul > li:nth-child(1)')
+}
 get customiseBtn(){
     return $('div.products-list-grid-container > ul > li:nth-child(1) > div > a')
 }
 get color(){
-    return $('div.pdp-selection-level-row__img.pdp-selection-level-row__img--active')
+    return $(' div.pdp-selection-level__flex > div.pdp-selection-level__row.pdp-selection-level-row > div:nth-child(3)')
 }
 get tone(){
     return $(' div.pdp-selection > div > div:nth-child(2) > div.pdp-selection-level__flex > div > div:nth-child(3)')
@@ -26,11 +32,70 @@ get addBagBtn(){
 get closeBtn(){
     return $ (' div.cart.cart--open.false > div > div.cart-header > button')
 }
+get elementCount(){
+    return $('div.cart-header > span.cart-header__count')
+}
+get bagBtn(){
+    return $('div.header__container > div:nth-child(3) > button')
+}
+get cartItem(){
+    return $ ('div.cart-items > ul > div > div > div.cart-item__header > p')
+}
+get removeBtn(){
+    return $('div.cart.cart--open.false > div > div.cart-items > ul > div > div > button')
+}
+get emptyCart(){
+    return $ ('div.cart.cart--open.cart--error > div > div.cart-empty > p')
+}
+get continueShoppingBtn(){
+    return $ ('div.cart.cart--open.cart--error > div > div.cart-empty > button')
+}
+get randomItem(){
+    return $ ('div.pdp-pair-with > div > ul > li:nth-child(2) > div > button')
+}
+get checkoutBtn(){
+    return $('div.cart-footer > div.cart-footer__buttons > button.cart-footer__checkout.cart__submit')
+}
+get skipPopup(){
+    return $('div.modal-upsell__footer.modal-upsell-footer > button')
+}
+get emailField(){
+    return $('#checkout_email')
+}
+get firstNameField(){
+    return $('#checkout_shipping_address_first_name')
+}
+get lastNameField(){
+    return $('#checkout_shipping_address_last_name')
+}
+get addressField(){
+    return $('#checkout_shipping_address_address1')
+}
+get cityField(){
+    return $('#checkout_shipping_address_city')
+}
+get postcodeField(){
+    return $('#checkout_shipping_address_zip')
+}
+get countryDropdown(){
+    return $('#checkout_shipping_address_country')
 
-
-
-
-
+}
+get shippingBtn(){
+    return $('#continue_button')
+}
+get errorMssgPost(){
+    return $('#error-for-zip')
+}
+get errorMssgAddrs(){
+    return $('#error-for-address1')
+}
+get addressFieldWrapper(){
+    return $('div[data-address-field="address1"]');
+}
+get postCodeWrapper(){
+    return $('div[data-address-field="zip"]')
+}
 
 
 } module.exports = new test();
--- test/specs/test.js
+++ test/specs/test.js
@@ -1,10 +1,53 @@
 const test = require('../pageobjects/test');
+//const Page = require('../pageobjects/page');
 
 
 describe('test for online shop', () => {
     it('the user should buy some items from online shop', async () => {
       await  browser.url("https://joshwoodcolour.com/")
-      
+      browser.pause(3000)
+      await test.popupDiscountCloseBtn.click();
+      await  test.allItems.click();
+      const elem = await $('div.products-list-grid-container > ul > li:nth-child(1)');
+      await elem.scrollIntoView();
+      browser.elementHover(elem)
+      await test.customiseBtn.click();
+      await test.color.click();
+      await test.tone.click();
+      await test.deliveryBtn.isClickable();
+      await test.deliveryBtn.click();
+      await test.addBagBtn.click();
+      await (test.elementCount).isEqual(1);
+      await test.closeBtn.click()
+      await test.bagBtn.click();
+      await expect (test.cartItem).toHaveTextContaining('he Miracle System - Permanent Colour 4.0 + Chocolate')
+      await test.removeBtn.click();
+      await expect (test.emptyCart).toHaveTextContaining('Your cart is currently empty.')
+      await test.continueShoppingBtn.click();
+      await test.randomItem.click();
+       $$('div.cart-item__info > div > span')[1].click();
+      await test.checkoutBtn.click();
+      await test.skipPopup.click();
+      await (test.emailField).setValue('javab78726@lankew.com')
+      const country = ['Isle of Man','Guernsey','Jersey']
+      const randomCountry = country[Math.floor(Math.random()*country.length)]
+      await (test.countryDropdown).selectByVisibleText(randomCountry)
+      await(test.firstNameField).setValue("Test");
+      await (test.lastNameField).setValue('Test1');
+      await (test.addressField).setValue('');
+      await(test.cityField).setValue('invalidCity');
+      await (test.postcodeField).setValue('1234');
+      await test.shippingBtn.click();
+      await expect (test.addressFieldWrapper).toHaveAttributeContaining('class','field--error')
+      await expect(test.postCodeWrapper).toHaveAttributeContaining('class',"field--error")
+      await expect(test.errorMssgAddrs).toHaveText('Enter an address');
+      await expect(test.errorMssgPost).toHaveText('Enter a valid postal code for ' + randomCountry);
+
+    
+
+
+
+
     });
 });
 
